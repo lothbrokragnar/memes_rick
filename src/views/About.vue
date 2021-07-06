e54d13aefe9237e9a8f70ce0615c7ea9d940492d
lothbrokragnar <gcuevas@sodimac.cl>
2021-07-06 12:04:32 -0400

    memes_rick

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <Personaje/>
+</template>
+<script>
+// @ is an alias to /src
+import Personaje from '../components/Personaje.vue'
+
+export default {
+  /* name: 'Home', */
+  components: {
+    Personaje
+  },
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListaPersonajes/>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListaPersonajes from '../components/ListaPersonajes.vue'
+
+export default {
+  /* name: 'Home', */
+  components: {
+    ListaPersonajes
+  },
+
+}
+</script>
